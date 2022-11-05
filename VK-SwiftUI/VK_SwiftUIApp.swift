909bd1663b77fe3750b7b222ad3b81df9259bf9c
Soplakov <soplakov@gmail.com>
2022-11-05 23:49:16 +0500

    Initial Commit

---
+++ VK-SwiftUI/VK_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VK_SwiftUIApp.swift
+//  VK-SwiftUI
+//
+//  Created by PiZero on 05.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct VK_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
