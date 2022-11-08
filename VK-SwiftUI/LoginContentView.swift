cea71a68873b703c6cb1e03b82056505becac6b2
Soplakov <soplakov@gmail.com>
2022-11-08 21:44:42 +0500

    Добавил метод исчезновения клавиатуры

--- VK-SwiftUI/LoginContentView.swift
+++ VK-SwiftUI/LoginContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 struct LoginContentView: View {
     
@@ -14,55 +15,74 @@ struct LoginContentView: View {
     
     @State private var shouldShowLogo: Bool = true
     
-    
+    private let keyboardIsOnPublisher = Publishers.Merge(
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification)
+            .map { _ in true },
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+            .map { _ in false } )
+        .removeDuplicates()
     
     
     var body: some View {
         
-        ScrollView() {
-        // Оборачиваем Stack в ScrollView что бы он скролился
-        VStack {
-            Image("vk")
-                .resizable()
-                .frame(width: 100, height: 100, alignment: .center)
-        Text("ВКонтакте")
-                .font(.largeTitle)
-                .bold()
-                .padding(.top, 32)
-        }
-        
-        VStack {
-            // Поле ввода логина пользователя
-            HStack {
-                Text("Login:")
-                Spacer()
-                TextField("", text: $login)
-                    .frame(maxWidth: 150)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+        ScrollView(showsIndicators: false) {
+            // Оборачиваем Stack в ScrollView что бы он скролился
+            VStack {
+                if shouldShowLogo {
+                    
+                    Image("vk")
+                        .resizable()
+                        .frame(width: 100, height: 100, alignment: .center)
+                    Text("ВКонтакте")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding(.top, 32)
+                }
+                
+                VStack {
+                    // Поле ввода логина пользователя
+                    HStack {
+                        Text("Login:")
+                        Spacer()
+                        TextField("", text: $login)
+                            .frame(maxWidth: 150)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                    // Поле ввода пароля пользователя
+                    HStack {
+                        Text("Password:")
+                        Spacer()
+                        SecureField("", text: $password)
+                            .frame(maxWidth: 150)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                } .frame(maxWidth: 250)
+                    .padding(.top, 50)
+                
+                Button(action: { print("Hello") }) {
+                    Text("Войти")
+                }
+                .padding(.top, 50)
+                .padding(.bottom, 20)
+                // Кнопка Войти будет недоступна если одно из полей будет пустое
+                .disabled(login.isEmpty || password.isEmpty)
             }
-            // Поле ввода пароля пользователя
-            HStack {
-                Text("Password:")
-                Spacer()
-                SecureField("", text: $password)
-                    .frame(maxWidth: 150)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-            }
-        } .frame(maxWidth: 250)
-            .padding(.top, 50)
-        
-        Button(action: { print("Hello") }) {
-            Text("Войти")
         }
-        .padding(.top, 50)
-        .padding(.bottom, 20)
-        // Кнопка Войти будет недоступна если одно из полей будет пустое
-        .disabled(login.isEmpty || password.isEmpty)
+        .onReceive(keyboardIsOnPublisher) { isKeyboardOn in withAnimation(Animation.easeInOut(duration: 0.5)) {
+            self.shouldShowLogo = !isKeyboardOn
+        }
+            
+        } .onTapGesture { UIApplication.shared.endEditing()
+        }
     }
 }
-}
-    
 
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
 
 struct LoginContentView_Previews: PreviewProvider {
     static var previews: some View {
